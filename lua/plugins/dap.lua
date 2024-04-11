bcfba30b42c17b85c4c03518c91b81195b5442af
Spandan Dutta <spdutta@microsoft.com>
2024-04-11 12:31:30 -0700

    changes

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,49 +1,128 @@
+-- Debugging.
 return {
   {
-    "rcarriga/nvim-dap-ui",
+    "mfussenegger/nvim-dap",
     dependencies = {
+      -- Fancy UI for the debugger
       {
-        "mfussenegger/nvim-dap",
+        "rcarriga/nvim-dap-ui",
+        dependencies = {
+          "nvim-neotest/nvim-nio",
+        },
+        opts = {
+          floating = { border = "rounded" },
+          layouts = {
+            {
+              elements = {
+                { id = "stacks", size = 0.30 },
+                { id = "breakpoints", size = 0.20 },
+                { id = "scopes", size = 0.50 },
+              },
+              position = "left",
+              size = 40,
+            },
+          },
+        },
       },
-      "nvim-neotest/nvim-nio",
-      "folke/neodev.nvim",
-
+      -- Virtual text.
+      {
+        "theHamsta/nvim-dap-virtual-text",
+        opts = {
+          virt_text_pos = "eol"
+        },
+      },
       -- Lua adapter.
       {
-        'jbyuki/one-small-step-for-vimkind',
+        "jbyuki/one-small-step-for-vimkind",
+        keys = {
+          {
+            "<leader>dl",
+            function()
+              require("osv").launch { port = 8086 }
+            end,
+            desc = "Launch Lua adapter",
+          },
+        },
       },
     },
     keys = {
-        {
-          '<leader>db',
-          function()
-            require('dap').toggle_breakpoint()
-          end,
-          desc = 'Toggle breakpoint',
-        },
-        {
-          '<leader>dl',
-          function()
-            require('osv').launch { port = 8086 }
-          end,
-          desc = 'Launch Lua adapter',
-        },
+      {
+        "<leader>db",
+        function()
+          require("dap").toggle_breakpoint()
+        end,
+        desc = "Toggle breakpoint",
+      },
+      {
+        "<leader>dB",
+        "<cmd>FzfLua dap_breakpoints<cr>",
+        desc = "List breakpoints",
+      },
+      {
+        "<leader>dc",
+        function()
+          require("dap").set_breakpoint(vim.fn.input "Breakpoint condition: ")
+        end,
+        desc = "Breakpoint condition",
+      },
+      {
+        "<F5>",
+        function()
+          require("dap").continue()
+        end,
+        desc = "Continue",
+      },
+      {
+        "<F10>",
+        function()
+          require("dap").step_over()
+        end,
+        desc = "Step over",
+      },
+      {
+        "<F11>",
+        function()
+          require("dap").step_into()
+        end,
+        desc = "Step into",
+      },
+      {
+        "<F12>",
+        function()
+          require("dap").step_out()
+        end,
+        desc = "Step Out",
+      },
     },
-    config = function ()
-      local dap = require('dap')
+    config = function()
+      local dap = require "dap"
+      local dapui = require "dapui"
+
+      -- Automatically open the UI when a new debug session is created.
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
 
       -- Lua configurations.
       dap.adapters.nlua = function(callback, config)
-        callback { type = 'server', host = config.host or '127.0.0.1', port = config.port or 8086 }
+        callback { type = "server", host = config.host or "127.0.0.1", port = config.port or 8086 }
       end
-      dap.configurations['lua'] = {
+      dap.configurations["lua"] = {
         {
-          type = 'nlua',
-          request = 'attach',
-          name = 'Attach to running Neovim instance',
-          port = 8086
+          type = "nlua",
+          request = "attach",
+          name = "Attach to running Neovim instance",
         },
       }
-    end
-  }
+    end,
+  },
 }
--- lua/plugins/sample.lua
+++ lua/plugins/sample.lua
@@ -1,6 +1,9 @@
 M = {}
 
+local a = 1 + 2
 print("Hello from sample file")
 print("This is a sample file to test require function in lua")
+
+print("The value of a is: ", a)
 
 return M
